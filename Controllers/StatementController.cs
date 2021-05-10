d2a70b2b7d686af8383e31e301063d9fd85af1ee
김성구 <cubist59@naver.com>
2021-05-10 12:16:16 +0900

    .

---
+++ Controllers/StatementController.cs
@@ -0,0 +1,26 @@
+﻿using ERP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ERP.Controllers
+{
+    public class StatementController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public StatementController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult StatementRegister()
+        {
+            return View();
+        }
+    }
+}
